04379f61ae9b59a5bbe09be34eb6652a8af7f250
Stadionova <726383@mail.ru>
2019-10-08 11:31:50 +0300

    broke down into jsx components

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className="header">
+        <img src='http://5nak.com/img/epic-1.png'></img>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className="content">
+        <div>
+            <img src="https://bipbap.ru/wp-content/uploads/2017/10/0_8eb56_842bba74_XL-640x400.jpg"></img>
+        </div>
+        <div>ava+desc</div>
+        <div>
+            my posts
+        <div
+            >new post
+        </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+                <div>
+                    post 3
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
