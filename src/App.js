e5c5b5aff6e2587180c615d7884e7b1f4e48953b
Stadionova <726383@mail.ru>
2019-10-09 23:12:03 +0300

    created other pages of navbar & added path in App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 // import Header from './Header';
 // import Header from './Header.js';
 // import Footer from './Footer';
@@ -30,6 +33,9 @@ const App = () => {
           {/* чтобы этого не было нужно указать путь path для каждого Route */}
           <Route path='/dialogs' component={Dialogs} />
           <Route path='/profile' component={Profile} />
+          <Route path='/news' component={News} />
+          <Route path='/music' component={Music} />
+          <Route path='/settings' component={Settings} />
           {/* <Profile /> */}
           {/* <Dialogs /> */}
         </div>
